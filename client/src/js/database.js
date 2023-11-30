4507d88deffc3a5de0f1c3c1ef74d0a5c6d54d1d
Anthony Nguyen <anthonynguye56@gmail.com>
2023-11-29 16:45:49 -0800

    minor fixes, line 21 changed content to value, line 23 and 37 removed .result, line 39 added ?.value to result

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,9 +18,9 @@ export const putDb = async (content) => {
   const jateDB = await openDB('jate', 1);
   const tx = jateDB.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({id: 1, content: content});
+  const request = store.put({id: 1, value: content});
 
-  const result = await request.result;
+  const result = await request;
   console.log(result);
 }
 
@@ -34,9 +34,9 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.get(1);
 
-  const result = await request.result;
+  const result = await request;
   console.log(result);
-  return result;
+  return result?.value;
 }
 
 
